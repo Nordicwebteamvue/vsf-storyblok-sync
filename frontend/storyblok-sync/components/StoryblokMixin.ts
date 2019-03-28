d357413d064b6ff59a117cc0fb20bfdfd772b2b2
Max Malm <benjick@dumfan.net>
2019-03-28 14:25:47 +0100

    Simplify mixin promises

--- frontend/storyblok-sync/components/StoryblokMixin.ts
+++ frontend/storyblok-sync/components/StoryblokMixin.ts
@@ -12,32 +12,19 @@ export default {
   asyncData ({ store, route, context }) {
     const paramString = route.fullPath.replace(route.path, '');
     const params = qs.parse(paramString, { ignoreQueryPrefix: true });
-    if (params._storyblok) {
-      // Fetch from storyblok
-      return new Promise((resolve, reject) => {
-        store.dispatch('storyblok/loadPreviewAsync', {
-          id: params._storyblok,
-          timestamp: params._storyblok_tk.timestamp,
-          spaceId: params.space_id
-        }).then(story => {
-          resolve({story});
-        }).catch(err => {
-          console.error(err)
-          reject(err)
-        })
-      })
-    }
-    return new Promise((resolve, reject) => {
-      // Fetch from API
-      store.dispatch('storyblok/loadAsync', {
-        id: route.fullPath
-      }).then(story => {
-        if (context) context.output.cacheTags.add(`storyblok`)
-        resolve({story})
-      }).catch(err => {
-        console.error(err)
-        reject(err)
-      })
+    const isEditing = !!params._storyblok;
+    const dispatch = isEditing ? store.dispatch('storyblok/loadPreviewAsync', {
+      id: params._storyblok,
+      timestamp: params._storyblok_tk.timestamp,
+      spaceId: params.space_id
+    }) : store.dispatch('storyblok/loadAsync', {
+      id: route.fullPath
+    })
+    return dispatch.then(story => {
+      if (context && !isEditing) {
+        context.output.cacheTags.add(`storyblok`)
+      }
+      return {story}
     })
   },
   methods: {
