751c086a0f64d9c4cb34fe96f2c131e1b9755d1e
Max Malm <benjick@dumfan.net>
2019-04-01 21:05:54 +0200

    Seed data before tests

--- cypress/integration/Home.spec.js
+++ cypress/integration/Home.spec.js
@@ -1,6 +1,9 @@
 /// <reference types="Cypress" />
 
 context('CI', () => {
+  beforeEach(() => {
+    cy.request('http://localhost:8080/api/ext/storyblok-sync/hook')
+  })
   it('can render blok', () => {
     cy.visit('http://localhost:3000/ci')
     cy.contains('ci=working')
