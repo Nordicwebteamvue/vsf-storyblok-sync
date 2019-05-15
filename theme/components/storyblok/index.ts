bd5bbb8058ad4f9b440f38469a3049aceddc1a5a
Simon Fridlund <simon@fridlund.email>
2019-05-15 11:10:23 +0200

    Add all components in theme

--- theme/components/storyblok/index.ts
+++ theme/components/storyblok/index.ts
@@ -5,3 +5,5 @@ add('demoImage', () => import('./Image.vue'))
 // Overwrite an existing component
 // add('tile', () => import('./Tile.vue'), { force: true })
 add('newsletter', () => import('./Newsletter.vue'))
+add('spacer', () => import('./Spacer.vue'))
+add('textSection', () => import('./TextSection.vue'))
