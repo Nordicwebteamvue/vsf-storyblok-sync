1dba9f398277bc27da4ae591b562c751487bd6d8
Anders Ekelund <anders.emil.ekelund@gmail.com>
2019-11-13 17:49:33 +0100

    Added global component for RichText

---
+++ packages/vsf-storyblok-module/components/global/RichTextResolver.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-html="html" />
+</template>
+
+<script>
+import { Blok } from 'src/modules/vsf-storyblok-module/components'
+import Vue from 'vue'
+
+export default {
+  name: 'RichTextResolver',
+  props: {
+    richText: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    html () {
+      return Vue.prototype.$storyblokClient.richTextResolver.render(this.richText)
+    }
+  }
+}
+</script>
