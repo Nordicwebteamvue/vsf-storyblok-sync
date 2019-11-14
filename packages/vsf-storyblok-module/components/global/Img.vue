0f692a3032f742c138d38320572c13f110a4f789
Andreas Karlsson <andreas@kodbruket.se>
2019-11-14 20:53:09 +0100

    Assume webp support Let server and initial state assume webp support to avoid loading both non-webp and webp image for clients supporting webp (80 % and growing, source https://caniuse.com/#feat=webp). This also results in the better performance score in Lighthouse tests.

--- packages/vsf-storyblok-module/components/global/Img.vue
+++ packages/vsf-storyblok-module/components/global/Img.vue
@@ -12,12 +12,15 @@
 <script>
 import { isServer } from '@vue-storefront/core/helpers'
 function canUseWebP () {
-  if (!isServer) {
-    const elem = document.createElement('canvas')
-    if ((elem.getContext && elem.getContext('2d'))) {
-      return elem.toDataURL('image/webp').indexOf('data:image/webp') === 0
-    }
+  if (isServer) {
+    return true;
   }
+
+  const elem = document.createElement('canvas')
+  if ((elem.getContext && elem.getContext('2d'))) {
+    return elem.toDataURL('image/webp').indexOf('data:image/webp') === 0
+  }
+
   return false
 }
 
