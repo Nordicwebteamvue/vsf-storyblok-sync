c06156b46a505135244395a9ec2d2f2fe1eeb60e
Max Malm <benjick@dumfan.net>
2019-11-27 14:59:57 +0100

    Remove storecode if using headers

--- packages/vsf-storyblok-module/components/global/RouterLink.vue
+++ packages/vsf-storyblok-module/components/global/RouterLink.vue
@@ -1,19 +1,39 @@
 <template>
-  <router-link v-if="link && link.linktype === 'story'" :to="'/' + link.cached_url">
+  <router-link v-if="link && link.linktype === 'story'" :to="url" class="link-cached-url">
     <slot />
   </router-link>
-  <a :href="link.url" v-else-if="link && link.url">
+  <a :href="link.url" v-else-if="link && link.url" class="link-linked-url">
     <slot />
   </a>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import config from 'config'
+import get from 'lodash-es/get'
+
 export default {
   name: 'StoryblokRouterLink',
   props: {
     link: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    ...mapState('storyblok', {
+      storeCodeFromHeader: (state) => state.storeCode
+    }),
+    url () {
+      let url = `/${this.link.cached_url}`
+      const addStoreCode = get(config, 'storyblok.settings.appendStoreCodeFromHeader')
+      if (this.link.linktype === 'story') {
+        if (addStoreCode && this.storeCodeFromHeader && url.startsWith(`/${this.storeCodeFromHeader}`)) {
+          return url.replace(`/${this.storeCodeFromHeader}`, '')
+        }
+        return url
+      }
+      return this.link.url
     }
   }
 }
