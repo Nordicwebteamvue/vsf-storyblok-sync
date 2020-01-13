6146c4a71324321a4de226b471a1bd5288cec100
Anders Ekelund <anders.emil.ekelund@gmail.com>
2020-01-13 16:27:54 +0100

    Add TLD support for meta hreflang tags. Also honor config storeview disabled setting.

--- packages/vsf-storyblok-module/pages/Storyblok.vue
+++ packages/vsf-storyblok-module/pages/Storyblok.vue
@@ -16,19 +16,33 @@ export default {
     const {hreflangPrefix} = getSettings(config.storyblok.settings)
     if (hreflangPrefix && this.story && this.story.alternates.length > 0) {
       const metaInfo = {
-        link: this.story.alternates.map(link => {
-          const storeCode = storeCodeFromRoute(link.full_slug)
+        link: this.story.alternates.filter(link => {
+          const storeCode = this.storeCodeFromSlug(link.full_slug)
+          return get(config.storeViews, [storeCode, 'disabled'], true) === false
+        })
+        .map(link => {
+          const storeCode = this.storeCodeFromSlug(link.full_slug)
           const locale = get(config.storeViews, [storeCode, 'i18n/defaultLocale'], storeCode)
+          const tldPrefix = get(config.storeViews, [storeCode, 'tld'], undefined)
+          const href = tldPrefix ? tldPrefix + '/' + removeStoreCodeFromSlug(link.full_slug) : hreflangPrefix + link.full_slug
           return {
             rel: 'alternate',
             hreflang: locale,
-            href: hreflangPrefix + link.full_slug
+            href: href
           }
         })
       }
       return metaInfo
     }
     return {}
+  },
+  methods: {
+    storeCodeFromSlug (slug) {
+      return slug.split(/\/(.+)/)[0]
+    },
+    removeStoreCodeFromSlug(slug) {
+      return slug.split(/\/(.+)/)[1]
+    }
   }
 }
 </script>
