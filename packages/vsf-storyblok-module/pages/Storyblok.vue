a0783b284b9511b975f7aea2348be7aa9802c5fb
Anders Ekelund <anders.emil.ekelund@gmail.com>
2020-01-14 17:51:25 +0100

    Remove dependency for config.storeViews.xx.tld

--- packages/vsf-storyblok-module/pages/Storyblok.vue
+++ packages/vsf-storyblok-module/pages/Storyblok.vue
@@ -8,6 +8,7 @@ import get from 'lodash.get'
 import { getSettings } from '../helpers'
 import storeCodeFromRoute from '@vue-storefront/core/lib/storeCodeFromRoute'
 import StoryblokMixin from '../components/StoryblokMixin'
+import { localizedRoute } from '@vue-storefront/core/lib/multistore'
 
 export default {
   name: 'StoryblokPage',
@@ -23,8 +24,8 @@ export default {
         .map(link => {
           const storeCode = this.storeCodeFromSlug(link.full_slug)
           const locale = get(config.storeViews, [storeCode, 'i18n/defaultLocale'], storeCode)
-          const tldPrefix = get(config.storeViews, [storeCode, 'tld'], undefined)
-          const href = tldPrefix ? tldPrefix + '/' + this.removeStoreCodeFromSlug(link.full_slug) : hreflangPrefix + link.full_slug
+          const storeViewUrl = get(config.storeViews, [storeCode, 'url'], '')
+          const href = this.isAbsoluteUrl(storeViewUrl) ? storeViewUrl + '/' + this.removeStoreCodeFromSlug(link.full_slug) : localizedRoute(link.full_slug)
           return {
             rel: 'alternate',
             hreflang: locale,
@@ -42,6 +43,9 @@ export default {
     },
     removeStoreCodeFromSlug(slug) {
       return slug.split(/\/(.+)/)[1]
+    },
+    isAbsoluteUrl (url) {
+      return /^[a-zA-Z][a-zA-Z\d+\-.]*:/.test(url)
     }
   }
 }
