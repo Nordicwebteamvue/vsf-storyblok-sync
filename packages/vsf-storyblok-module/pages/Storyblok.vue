bc20f758f4a5292b42cd9ac23894532f34463bd4
Anders Ekelund <anders.emil.ekelund@gmail.com>
2020-01-15 10:16:53 +0100

    Add TLD support for meta hreflang tags and honor storeView "disabled" setting (#86)
    * Add TLD support for meta hreflang tags. Also honor config storeview disabled setting.
    
    * Fix missing 'this' reference...
    
    * Remove dependency for config.storeViews.xx.tld
    
    * Remove unused import
    
    * Update packages/vsf-storyblok-module/pages/Storyblok.vue
    
    Co-authored-by: Max Malm <benjick@dumfan.net>

--- packages/vsf-storyblok-module/pages/Storyblok.vue
+++ packages/vsf-storyblok-module/pages/Storyblok.vue
@@ -6,8 +6,8 @@
 import config from 'config'
 import get from 'lodash.get'
 import { getSettings } from '../helpers'
-import storeCodeFromRoute from '@vue-storefront/core/lib/storeCodeFromRoute'
 import StoryblokMixin from '../components/StoryblokMixin'
+import { localizedRoute } from '@vue-storefront/core/lib/multistore'
 
 export default {
   name: 'StoryblokPage',
@@ -16,19 +16,36 @@ export default {
     const {hreflangPrefix} = getSettings(config.storyblok.settings)
     if (hreflangPrefix && this.story && this.story.alternates.length > 0) {
       const metaInfo = {
-        link: this.story.alternates.map(link => {
-          const storeCode = storeCodeFromRoute(link.full_slug)
+        link: this.story.alternates.filter(link => {
+          const storeCode = this.storeCodeFromSlug(link.full_slug)
+          return get(config.storeViews, [storeCode, 'disabled'], true) === false
+        })
+        .map(link => {
+          const storeCode = this.storeCodeFromSlug(link.full_slug)
           const locale = get(config.storeViews, [storeCode, 'i18n/defaultLocale'], storeCode)
+          const storeViewUrl = get(config.storeViews, [storeCode, 'url'], '')
+          const href = this.isAbsoluteUrl(storeViewUrl) ? storeViewUrl + '/' + this.removeStoreCodeFromSlug(link.full_slug) : localizedRoute(link.full_slug)
           return {
             rel: 'alternate',
             hreflang: locale,
-            href: hreflangPrefix + link.full_slug
+            href
           }
         })
       }
       return metaInfo
     }
     return {}
+  },
+  methods: {
+    storeCodeFromSlug (slug) {
+      return slug.split(/\/(.+)/)[0]
+    },
+    removeStoreCodeFromSlug(slug) {
+      return slug.split(/\/(.+)/)[1]
+    },
+    isAbsoluteUrl (url) {
+      return /^[a-zA-Z][a-zA-Z\d+\-.]*:/.test(url)
+    }
   }
 }
 </script>
