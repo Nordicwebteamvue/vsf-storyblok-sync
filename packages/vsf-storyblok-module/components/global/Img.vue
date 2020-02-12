989110ca89231891242cd4677bb2e672fcbbf08e
Max Malm <benjick@dumfan.net>
2020-02-12 18:23:22 +0100

    Add support for pinterest mode and using element width

--- packages/vsf-storyblok-module/components/global/Img.vue
+++ packages/vsf-storyblok-module/components/global/Img.vue
@@ -1,8 +1,8 @@
 <template>
-  <div v-if="div && lazy" v-lazy:background-image="image" :style="{ backgroundImage: `url('${placeholder}')` }">
+  <div ref="element" v-if="div && lazy" v-lazy:background-image="image" :style="{ backgroundImage: `url('${placeholder}')` }">
     <slot />
   </div>
-  <div v-else-if="div" :style="{ backgroundImage: `url('${image}')` }">
+  <div ref="element" v-else-if="div" :style="{ backgroundImage: `url('${image}')` }">
     <slot />
   </div>
   <img v-else-if="lazy" v-lazy="image" :src="placeholder">
@@ -13,9 +13,63 @@
 import get from 'lodash-es/get'
 import config from 'config'
 import { mapGetters } from 'vuex'
+import { isServer } from '@vue-storefront/core/helpers'
+import debounce from 'lodash-es/debounce'
+
+const baseUrl = 'https://img2.storyblok.com'
 
 export default {
   name: 'StoryblokImage',
+  data () {
+    return {
+      elementWidth: 0
+    }
+  },
+  mounted () {
+    if (this.div && this.useElementWidth) {
+      this.elementWidth = this.$refs.element.offsetWidth
+    }
+  },
+  created () {
+    if (!isServer && this.div && this.useElementWidth) {
+      window.addEventListener('resize', debounce(this.onResize, 250))
+    }
+  },
+  beforeDestroy () {
+    if (!isServer) {
+      window.removeEventListener('resize', debounce(this.onResize, 250))
+    }
+  },
+  methods: {
+    onResize () {
+      this.elementWidth = this.$refs.element.offsetWidth
+    },
+    createUrl (url, filters = [], { pinterest } = {}) {
+      const [, resource] = url.split('//a.storyblok.com')
+      let mod = ''
+      // const width = this.useElementWidth ? this.elementWidth : this.width
+      if (pinterest) {
+        return baseUrl + '/1x1' + resource
+      }
+      if (this.height > 0 || this.width > 0 || this.useElementWidth) {
+        if (this.fitIn) {
+          mod += '/fit-in'
+        }
+        if (this.useElementWidth) {
+          mod += `/${this.elementWidth}x0`
+        } else {
+          mod += `/${this.width}x${this.height}`
+        }
+        if (this.smart) {
+          mod += '/smart'
+        }
+      }
+      if (filters.length) {
+        mod += '/filters:' + filters.join(':')
+      }
+      return baseUrl + mod + resource
+    }
+  },
   computed: {
     ...mapGetters({
       supportsWebp: 'storyblok/supportsWebp'
@@ -30,27 +84,28 @@ export default {
       if (!this.src.includes('//a.storyblok.com')) {
         return this.src
       }
-      const [, resource] = this.src.split('//a.storyblok.com')
-      let mod = ''
-      if (this.height > 0 || this.width > 0) {
-        if (this.fitIn) {
-          mod += '/fit-in'
-        }
-        mod += `/${this.width}x${this.height}`
-        if (this.smart) {
-          mod += '/smart'
+      if (this.pinterest && this.lazy) {
+        return {
+          src: this.createUrl(this.src, this.computedFilters),
+          error: this.src,
+          loading: this.createUrl(this.src, this.computedFilters, { pinterest: true })
         }
       }
-      if (this.computedFilters.length) {
-        mod += '/filters:' + this.computedFilters.join(':')
-      }
-      return 'https://img2.storyblok.com' + mod + resource
+      return this.createUrl(this.src, this.computedFilters)
     }
   },
   props: {
     placeholder: {
       type: String,
       default: 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='
+    },
+    pinterest: {
+      type: Boolean,
+      default: false
+    },
+    useElementWidth: {
+      type: Boolean,
+      default: false
     },
     detectWebp: {
       type: Boolean,
