58d8f104ad6c2b948f125e4d18a0bba924668c3d
Max Malm <benjick@dumfan.net>
2020-02-14 11:55:10 +0100

    No cache

--- packages/vsf-storyblok-extension/index.js
+++ packages/vsf-storyblok-extension/index.js
@@ -30,7 +30,7 @@ module.exports = ({ config, db }) => {
   const storyblokClientConfig = {
     accessToken: config.storyblok.previewToken,
     cache: {
-      type: 'memory'
+      type: 'none'
     }
   }
   const cache = cacheFactory(config)
