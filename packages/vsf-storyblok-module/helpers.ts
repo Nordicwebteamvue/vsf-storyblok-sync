d6551bccff4d90197e6e6e25391272cb94df303e
Max Malm <benjick@dumfan.net>
2020-02-17 11:31:07 +0100

    Assign script id after load

--- packages/vsf-storyblok-module/helpers.ts
+++ packages/vsf-storyblok-module/helpers.ts
@@ -11,11 +11,11 @@ export function loadScript (src: string, id: string) {
     var script = document.createElement('script')
     script.async = true
     script.src = src
-    script.id = id
     script.onerror = function onError () {
       reject(new Error('Failed to load' + src))
     }
     script.onload = function onLoad () {
+      script.id = id
       resolve()
     }
     document.getElementsByTagName('head')[0].appendChild(script)
