1194452a55b3bf7220d0e35e94484dcdaa3aa525
Max Malm <benjick@dumfan.net>
2020-02-18 08:58:50 +0100

    Enable headers from server request

--- packages/vsf-storyblok-module/hooks/afterRegistration.ts
+++ packages/vsf-storyblok-module/hooks/afterRegistration.ts
@@ -1,11 +1,13 @@
-function afterRegistration ({ isServer, Vue, ...rest }) {
-  if (isServer) {
-    // const { request: req } = Vue.prototype.$ssrRequestContext.server
-    // if (req.headers['x-vs-store-code']) {
-    //   store.dispatch('storyblok/setStoreCode', req.headers['x-vs-store-code'])
-    // }
-    // const supportsWebp = req.headers.accept.includes('image/webp')
-    // store.commit('storyblok/supportsWebp', supportsWebp)
+import has from 'lodash-es/has'
+
+function afterRegistration ({ isServer, Vue, store }) {
+  if (isServer && has(Vue, 'prototype.$ssrRequestContext.server.request')) {
+    const { request } = Vue.prototype.$ssrRequestContext.server
+    if (request.headers['x-vs-store-code']) {
+      store.dispatch('storyblok/setStoreCode', request.headers['x-vs-store-code'])
+    }
+    const supportsWebp = request.headers.accept.includes('image/webp')
+    store.commit('storyblok/supportsWebp', supportsWebp)
   }
 }
 export { afterRegistration }
