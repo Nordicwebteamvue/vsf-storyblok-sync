51df6d01df065deda7a542eb39bd2309ff1622bd
Max Malm <benjick@dumfan.net>
2020-02-19 08:40:19 +0100

    Add test for richtext

---
+++ theme/components/storyblok/RichText.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <sb-rich-text v-if="item.text" :text="item.text" />
+  </div>
+</template>
+
+<script>
+import { Blok } from 'src/modules/vsf-storyblok-module/components'
+export default {
+  name: 'RichTextBlok',
+  extends: Blok
+}
+</script>
