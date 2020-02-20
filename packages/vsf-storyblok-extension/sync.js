1d8b073306b445fc45be6d6fcaec050c886ead6e
Max Malm <benjick@dumfan.net>
2020-02-20 08:33:41 +0100

    Restructure files

--- packages/vsf-storyblok-extension/sync.js
+++ packages/vsf-storyblok-extension/sync.js
@@ -1,5 +1,5 @@
 import { storyblokClient } from './storyblok'
-import { log, createIndex, createBulkOperations } from './helpers'
+import { log, createIndex, createBulkOperations, transformStory, cacheInvalidate } from './helpers'
 
 function indexStories ({ db, stories = [] }) {
   const bulkOps = createBulkOperations(stories)
@@ -39,4 +39,35 @@ const fullSync = async (db, config) => {
   await syncStories({ db, perPage: config.storyblok.perPage })
 }
 
-export { syncStories, fullSync }
+const handleHook = async (db, config, params) => {
+  const cv = Date.now() // bust cache
+  const { story_id: id, action } = params
+
+  switch (action) {
+    case 'published':
+      const { data: { story } } = await storyblokClient.get(`cdn/stories/${id}`, {
+        cv,
+        resolve_links: 'url'
+      })
+      const publishedStory = transformStory(story)
+
+      await db.index(publishedStory)
+      log(`Published ${story.full_slug}`)
+      break
+
+    case 'unpublished':
+      const unpublishedStory = transformStory({ id })
+      await db.delete(unpublishedStory)
+      log(`Unpublished ${id}`)
+      break
+
+    case 'branch_deployed':
+      await fullSync(db, config)
+      break
+    default:
+      break
+  }
+  await cacheInvalidate(config.storyblok)
+}
+
+export { syncStories, fullSync, handleHook }
